ea83426da4903bc3c510c33b629bd7a1ba37436a
nickforlinux <inal.kharayev@outlook.com>
2025-03-02 13:43:25 +0300

    add loader settings

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,14 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
 import LoaderComponent from './components/LoaderComponent.vue'
+import { ref } from 'vue'
+const isLoading = ref(true)
+
+setTimeout(() => {
+  isLoading.value = false
+}, 3000)
 </script>
 <template>
   <RouterView />
-  <LoaderComponent />
+  <LoaderComponent v-if="isLoading" />
 </template>
