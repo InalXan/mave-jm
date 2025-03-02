2f91f358d3a609d040c228e8ca4fe35aba11ad55
nickforlinux <inal.kharayev@outlook.com>
2025-03-02 12:59:35 +0300

    add loader page introduction

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,8 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
+import LoaderComponent from './components/LoaderComponent.vue'
 </script>
 <template>
   <RouterView />
+  <LoaderComponent />
 </template>
---
+++ src/components/LoaderComponent.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="fixed inset-0 flex justify-center items-center bg-second z-50">
+    <div class="group">
+      <div class="relative flex justify-center items-center">
+        <!-- Logo -->
+        <img src="@/assets/images/logo/logo.png" class="w-48" alt="" />
+        <!-- Spinner -->
+        <div
+          class="absolute w-16 h-16 border-4 border-t-transparent border-blue-500 rounded-full animate-spin opacity-0 group-hover:opacity-100 transition-opacity duration-500"
+        ></div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { ref, onMounted } from 'vue'
+
+const isLoading = ref(true)
+
+onMounted(() => {
+  setTimeout(() => {
+    isLoading.value = false
+  }, 3000)
+})
+</script>
